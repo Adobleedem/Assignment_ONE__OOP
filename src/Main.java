c6e573d6863c97ee4c4d9e54477bfb9e2aa78701
Ado <bigado39@gmail.com>
2026-04-26 14:45:42 +0300

    Final project code

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    int Num1 = 8;
+    int Num2 = 16;
+
+    int sum = Num1 + Num2;
+
+        System.out.println(sum);
+    }
+}
